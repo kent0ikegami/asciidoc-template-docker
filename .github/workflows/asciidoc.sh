26257822a1d8801f445b0333f8290f1b48e7649c
ikegami <kent0ikegamixxx@gmail.com>
2022-10-16 17:53:31 +0900

    aaaa

--- .github/workflows/asciidoc.sh
+++ .github/workflows/asciidoc.sh
@@ -2,7 +2,6 @@
 set -e
 
 mkdir -p ./dist/
-mkdir -p ./themes/
 
 CURRENT_PATH=`pwd`
 ASCIIDOCTOR_PDF_DIR=`gem contents asciidoctor-pdf --show-install-dir`
@@ -10,6 +9,8 @@ ASCIIDOCTOR_PDF_DIR=`gem contents asciidoctor-pdf --show-install-dir`
 # cp "./src/resource/themes/default-theme.yml" ${CURRENT_PATH}/themes/default-theme.yml
 # cp -r -f "./src/resource/fonts/" ${CURRENT_PATH}/
 
+cp "${ASCIIDOCTOR_PDF_DIR}/data/themes/default-theme.yml" ${CURRENT_PATH}/src/resource/themes/default-theme.yml
+cp -r -f "${ASCIIDOCTOR_PDF_DIR}/data/fonts/" ${CURRENT_PATH}/src/resource/fonts/
 
 # -a, --attribute=ATTRIBUTE
 # -B, --base-dir=DIR
@@ -22,6 +23,6 @@ ASCIIDOCTOR_PDF_DIR=`gem contents asciidoctor-pdf --show-install-dir`
 
 asciidoctor -D ./dist/ -o index.html -r asciidoctor-diagram ./src/index.adoc
 
-asciidoctor-pdf -a pdf-theme=./resource/themes/default-theme.yml -a pdf-fontsdir=./resource/fonts -a source-highlighter=pygments -r ./src/resource/patch-prawn.rb -D ./dist/ -o index.pdf -a scripts@=cjk -r asciidoctor-diagram ./src/index.adoc
+asciidoctor-pdf -a df-styledir=${CURRENT_PATH}/src/resource/themes/ -a pdf-fontsdir=${CURRENT_PATH}/src/resource/fonts -a source-highlighter=pygments -r ./src/resource/patch-prawn.rb -D ./dist/ -o index.pdf -a scripts@=cjk -r asciidoctor-diagram ./src/index.adoc
 # -a pdf-theme=./src/resource/theme-pdf.yml -a pdf-fontsdir=./src/resource/fonts
 cp -rf ./src/images/ ./dist/images/
