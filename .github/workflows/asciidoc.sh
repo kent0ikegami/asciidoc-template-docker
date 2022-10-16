18d43e4c40efd0ca6b19a0ffe647b8e7ac892439
ikegami <kent0ikegamixxx@gmail.com>
2022-10-16 15:59:22 +0900

    4

---
+++ .github/workflows/asciidoc.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+mkdir -p ./dist/
+
+CURRENT_PATH=`pwd`
+ASCIIDOCTOR_PDF_DIR=`gem contents asciidoctor-pdf --show-install-dir`
+
+cp "${ASCIIDOCTOR_PDF_DIR}/data/themes/default-theme.yml" ${CURRENT_PATH}/themes/default-theme.yml
+cp -r -f "${ASCIIDOCTOR_PDF_DIR}/data/fonts/" ${CURRENT_PATH}/
+
+
+# -a, --attribute=ATTRIBUTE
+# -B, --base-dir=DIR
+# -D, --destination-dir=DIR
+# -o, --out-file=OUT_FILE
+# -R, --source-dir=DIR
+# -b, --backend=BACKEND
+# -d, --doctype=DOCTYPE
+# -r, --require=LIBRARY
+
+asciidoctor -B ${CURRENT_PATH}/src/ -D ${CURRENT_PATH}/dist/ -o index.html -r asciidoctor-diagram index.adoc
+
+# asciidoctor-pdf -a pdf-theme=assets/theme-pdf.yml -a pdf-fontsdir=assets/fonts -B ${CURRENT_PATH}/src/ -D ${CURRENT_PATH}/dist/ -o index.pdf -a scripts@=cjk -r asciidoctor-diagram index.adoc
+
+cp -rf ./src/images/ ./dist/images/
