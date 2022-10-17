e54bdb1bdf2a4e68bc7cd4b6dfd8d51631dcbda6
ikegami <kent0ikegamixxx@gmail.com>
2022-10-17 22:07:35 +0900

    ss

--- .github/workflows/asciidoc.sh
+++ .github/workflows/asciidoc.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
-mkdir -p ./dist/
-
-cp -rf ./src/images/ ./dist/
-
 CURRENT_PATH=`pwd`
 
+mkdir -p ./dist/
+
+# htmlの表示用と、pdfの埋め込み用に、imagesをdistに配置(pdfは出力時のみ参照)
+cp -rf ./src/images/ ./dist/
+
+# drawioで出力するSVGのfont-familyを一括変換 Helvetica -> GenShinGothic-P
 dir_path="${CURRENT_PATH}/dist/images/*"
 dirs=`find $dir_path -maxdepth 0 -type f -name *.drawio.svg`
 for dir in $dirs;
@@ -14,7 +16,10 @@ do
   sed -i 's/font-family="Helvetica"/font-family="GenShinGothic-P"/g' $dir
 done
 
+# html出力
 asciidoctor -D ./dist/ -o index.html -r asciidoctor-diagram ${CURRENT_PATH}/src/index.adoc
+
+# pdf出力
 asciidoctor-pdf \
   -a pdf-theme=${CURRENT_PATH}/src/resource/user-theme.yml \
   -a pdf-fontsdir=${CURRENT_PATH}/src/resource/fonts \
